cce6f4bc0e246a3132165266617151b5570003f5
choephix.c9 <i@thechoephix.com>
2018-06-24 14:43:35 +0000

    multiple str + node actions + pagination

--- src/providers/gamestate.ts
+++ src/providers/gamestate.ts
@@ -42,7 +42,20 @@ export class GameState {
     let options = [];
     
     if ( "on_look" in node_data )
-      options.push( { link_text : "Inspect "+node_data.title, on_chosen : ()=>this.addJournalEntry(node_data.on_look) } );
+    {
+      options.push( { 
+        link_text : this.random(["Look around","Inspect "+node_data.title]), 
+        on_chosen : ()=>this.addJournalEntry(this.random(node_data.on_look))
+      } );
+    }
+    
+    if ( "actions" in node_data ) for ( let action of node_data.actions )
+    {
+      options.push( { 
+        link_text : action.handle as string, 
+        on_chosen : ()=>this.performAction( action.on_do, options ) 
+      } )
+    }
     
     for ( let exit of node_data.exits )
     {
@@ -52,54 +65,37 @@ export class GameState {
       } )
     }
     
-    options.push( { link_text : "...", on_chosen : ()=>{} } );
-    options.push( { link_text : "(Reflect on your life)", on_chosen : ()=>this.sleep() } );
+    options.push( { link_text : "(Reflect on your life)", on_chosen : ()=>{} } );
       
     this.setOptions( options );
   }
-
-  sleep():void {
-    let $this = this
-    this.addJournalEntry("You sleep.")
-    this.setOptions(
-      [
-        { link_text : ">>", on_chosen : ()=>step2() },
-      ]
-    )
-
-    function step2()
-    {
-      $this.addJournalEntry("And you sleep.")
-      $this.setOptions(
-        [
-          { link_text : ">>", on_chosen : ()=>step3() },
-        ]
-      )
+  
+  private performAction(journalEntries:string[],options:Option[]):void {
+    
+    journalEntries = Object.assign([], journalEntries)
+    
+    let next:()=>void = () => {
+      this.addJournalEntry( journalEntries.shift() );
+      if ( journalEntries.length > 0 )
+        this.setOptions( [ { link_text : ">>", on_chosen : ()=>next() } ] )
+      else
+        this.setOptions( options )
     }
-
-    function step3()
-    {
-      $this.addJournalEntry("And...")
-      $this.setOptions(
-        [
-          { link_text : ">>", on_chosen : ()=>$this.reset() },
-        ]
-      )
-    }
+    
+    next();
   }
 
-  addJournalEntry(s:string):void {
-    
+  private addJournalEntry(s:string):void {
     if( this.current_journal_entry != undefined )
       this.journal.push(this.current_journal_entry)
     this.current_journal_entry = s;
     console.log(":: " + s)
-    
-    try { this.on_change() }
-    catch(e) { console.log(e) }
+    this.on_change()
   }
 
-  setOptions(list:Option[]):void {
+  private random(s:string[]):string { return s[Math.floor(Math.random()*s.length)] }
+
+  private setOptions(list:Option[]):void {
     this.options = list
   }
 
